84fa234aa228517bcc96800a5572962ec856d0f8
Sintijaa <144904166+Sintijaa@users.noreply.github.com>
2024-09-17 12:32:41 +0300

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,31 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
 import './App.css';
-import sakuma_bilde from './assets/sakuma_bilde.jpeg';
+import Shop from './Components/Shop';  
 
-function App() {
+function Home() {
+  const navigate = useNavigate();
+
   return (
     <div className="background">
       <div className="content">
         <h1>Sun Aroma</h1>
-        <button>Doties uz veikalu</button>
+        <button onClick={() => navigate('/shop')}>Doties uz veikalu</button>
       </div>
     </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* Define route for the Home component */}
+        <Route path="/" element={<Home />} />
+        {/* Define route for the Shop component */}
+        <Route path="/shop" element={<Shop />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ frontend/src/Components/shop.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../style/Shop.css';
+
+function Shop() {
+  return (
+    <div>
+      <h1>Kategorijas</h1>
+      
+    </div>
+  );
+}
+
+export default Shop;
