d3e23b640d344624849e9e5cba63ef3c3d228670
Sintijaa <144904166+Sintijaa@users.noreply.github.com>
2024-09-18 08:42:35 +0300

    .

--- frontend/src/Components/shop.jsx
+++ frontend/src/Components/shop.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
 import '../style/Shop.css';
+import epoksids from '../assets/epoksids.jpg';
+import sveces from '../assets/sveces.jpg'; 
+import vasks from '../assets/vasks.jpg';
+import burti from '../assets/burti.jpg';
 
 function Shop() {
   return (
     <div>
       <h1>Kategorijas</h1>
-      
+      <div className="image-gallery">
+        <div className="image-item">
+          <img src={epoksids} alt="Epoksīds" />
+          <p>Epoksīda dekori</p>
+        </div>
+        <div className="image-item">
+          <img src={sveces} alt="Sveces" />
+          <p>Sveces</p>
+        </div>
+        <div className="image-item">
+          <img src={vasks} alt="Vasks" />
+          <p>Aromātiskais vasks</p>
+        </div>
+        <div className="image-item">
+          <img src={burti} alt="Burti" />
+          <p>Epoksīda burti</p>
+        </div>
+      </div>
     </div>
   );
 }
-
+ 
 export default Shop;
