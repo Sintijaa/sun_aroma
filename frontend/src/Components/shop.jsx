b0ca0b3071224318edda22705d2d01d43eea9111
Sintijaa <144904166+Sintijaa@users.noreply.github.com>
2024-09-19 08:37:22 +0300

    .

--- frontend/src/Components/shop.jsx
+++ frontend/src/Components/shop.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../style/Shop.css';
 import epoksids from '../assets/epoksids.jpg';
-import sveces from '../assets/sveces.jpg'; 
+import sveces from '../assets/sveces.jpg';
 import vasks from '../assets/vasks.jpg';
 import burti from '../assets/burti.jpg';
 
@@ -11,40 +12,48 @@ function Shop() {
       <h1>Kategorijas</h1>
       <div className="image-gallery">
         <div className="image-item">
-          <img src={epoksids} alt="Epoksīds" />
-          <div class="container">
-            <button class="button type1">
-            Epoksīda dekori
-            </button>
-          </div>
+          <Link to="/dekori">
+            <img src={epoksids} alt="Epoksīds" />
+            <div className="container">
+              <button className="button type1">
+                Epoksīda dekori
+              </button>
+            </div>
+          </Link>
         </div>
         <div className="image-item">
-          <img src={sveces} alt="Sveces" />
-          <div class="container">
-            <button class="button type1">
-            Sveces
-            </button>
-          </div>
+          <Link to="/sveces">
+            <img src={sveces} alt="Sveces" />
+            <div className="container">
+              <button className="button type1">
+                Sveces
+              </button>
+            </div>
+          </Link>
         </div>
         <div className="image-item">
-          <img src={vasks} alt="Vasks" />
-          <div class="container">
-            <button class="button type1">
-            Aromātiskais vasks
-            </button>
-          </div>
+          <Link to="/vaski">
+            <img src={vasks} alt="Vasks" />
+            <div className="container">
+              <button className="button type1">
+                Aromātiskais vasks
+              </button>
+            </div>
+          </Link>
         </div>
         <div className="image-item">
-          <img src={burti} alt="Burti" />
-          <div class="container">
-            <button class="button type1">
-            Epoksīda burti
-            </button>
-          </div>
+          <Link to="/burti">
+            <img src={burti} alt="Burti" />
+            <div className="container">
+              <button className="button type1">
+                Epoksīda burti
+              </button>
+            </div>
+          </Link>
         </div>
       </div>
     </div>
   );
 }
- 
+
 export default Shop;
