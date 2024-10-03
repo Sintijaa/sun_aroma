31a9b1ae68ab832f4bd5cd8095f0f7e0a9f5c80b
Sintijaa <144904166+Sintijaa@users.noreply.github.com>
2024-10-03 08:38:27 +0300

    .

--- frontend/src/Components/Sveces.jsx
+++ frontend/src/Components/Sveces.jsx
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
-import '../style/Sveces.css'; // Tā pati CSS faila saite
+import '../style/Sveces.css';
 import kvarcs from '../assets/kvarcs.jpg';
 import ametists from '../assets/ametists.jpg';
 
+
 function Sveces() {
+   
+
     return (
         <div className="sveces-container">
-         <Link to="/shop" className="home-button">Sākums</Link> 
+            <Link to="/shop" className="home-button">Sākums</Link>
 
             <h1>Dizaina sveces</h1>
             <div className="galerija">
                 <div className="svece">
                     <img src={kvarcs} alt="kvarcs" className="svece-bilde" />
-                    <p className="svece-apraksts">Sojas vaska svece ar dabīgiem akmeņiem-Kvarcs</p>
+                    <p className="svece-apraksts">Sojas vaska svece ar dabīgiem akmeņiem - Kvarcs</p>
+                    <button className="add-to-cart-button" onClick={() => addToCart('Kvarcs')}>Pievienot grozam</button>
                 </div>
                 <div className="svece">
                     <img src={ametists} alt="ametists" className="svece-bilde" />
-                    <p className="svece-apraksts">Sojas vaska svece ar dabīgiem akmeņiem-Ametists</p>
+                    <p className="svece-apraksts">Sojas vaska svece ar dabīgiem akmeņiem - Ametists</p>
+                    <button className="add-to-cart-button" onClick={() => addToCart('Ametists')}>Pievienot grozam</button>
                 </div>
             </div>
         </div>
