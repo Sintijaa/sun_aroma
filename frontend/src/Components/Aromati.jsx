255418ed86ae7a2452226a1e0d87bc108ba1491f
Sintijaa <144904166+Sintijaa@users.noreply.github.com>
2024-10-03 12:48:54 +0300

    .

--- frontend/src/Components/Aromati.jsx
+++ frontend/src/Components/Aromati.jsx
@@ -7,24 +7,25 @@ import aromats3 from '../assets/aromats3.jpg';
 
 function Sveces() {
     return (
-        <div className="aromaticontainer">
-            <Link to="/shop" className="home-button">Sākums</Link> {/* Poga uz veikalu */}
-            <div className="galerija">
-            <h1>Aromāti</h1>
-                <div className="aromats">
-                    <img src={aromats1} alt="aromats1" className="aromats-bilde" />
-                    <p className="aromats-apraksts">Aromāts 1</p>
-                </div>
-                <div className="aromats">
-                    <img src={aromats2} alt="aromats2" className="aromats-bilde" />
-                    <p className="aromats-apraksts">Aromāts 2</p>
-                </div>
-                <div className="aromats">
-                    <img src={aromats3} alt="aromats3" className="aromats-bilde" />
-                    <p className="aromats-apraksts">Aromāts 3</p>
-                </div>
+        <div className="aromati-container">
+        <Link to="/shop" className="home-button">Sākums</Link>
+
+        <h1>Mājas un automašīnas aromāti</h1>
+        <div className="galerija">
+            <div className="auskari-item">
+                <img src={aromats1} alt="aromats1" className="auskari-bilde" />
+                <p className="auskari-apraksts">Epoksīda sveķu auskari ar sudraba folliju</p>
+            </div>
+            <div className="auskari-item">
+                <img src={aromats2} alt="aromats2" className="auskari-bilde" />
+                <p className="auskari-apraksts">Auskari ar kaltētiem ziediem</p>
+            </div>
+            <div className="auskari-item">
+                <img src={aromats3} alt="aromats3tfgrtfghg" className="auskari-bilde" />
+                <p className="auskari-apraksts">Auskari kas atgādinās par vasaru pat ziemā</p>
             </div>
         </div>
+    </div>
     );
 }
 
